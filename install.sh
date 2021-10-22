7b2d0a4e2f696df587631efcb37c5c4d4f8d4c54
The TwilightBlood <hwengerstickel@protonmail.com>
2021-10-21 19:53:22 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+wget -qO - 'https://proget.hunterwittenborn.com/debian-feeds/makedeb.pub' | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/makedeb-archive-keyring.gpg &> /dev/null
+echo 'deb [signed-by=/usr/share/keyrings/makedeb-archive-keyring.gpg arch=all] https://proget.hunterwittenborn.com/ makedeb main' | \
+sudo tee /etc/apt/sources.list.d/makedeb.list
+sudo apt update
+sudo apt makedeb
+git clone https://mpr.hunterwittenborn.com/stoke-git.git
+cd stoke-git
+makedeb -si
